d14d803dbc8e735ed254c141d5107340e3f13346
Sumit Pandey <heerjnv@gmail.com>
2024-10-01 08:31:04 +0530

    Cors Issue

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false, // Set to true if you need to allow credentials
   })
 );
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Routes
+// Routes
 app.use("/api/search", require("./Routes/SearchRoutes"));
 app.use("/api/files", require("./Routes/FilesRoutes"));
 
