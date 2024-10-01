fae59e00c4dde427df10b2a3890a8d1f1c7d8124
Sumit Pandey <heerjnv@gmail.com>
2024-10-01 08:17:24 +0530

    Cors Policy Change

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 
 const app = express();
 const port = 3000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
